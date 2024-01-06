ee69fab82b1ed283021dc932e95347cf643ad875
Luan Hernandes Batista <93232640+luanzio@users.noreply.github.com>
2024-01-05 22:18:43 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:3001',
+  experimental: {
+    appDir: true,
   },
 };
 
